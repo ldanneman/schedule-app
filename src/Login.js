1e3e5e5448b82396c9b267364a1a8379b1397891
Avishaignd <avishaignd@gmail.com>
2021-01-20 12:15:19 +0200

    login WIP

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,15 @@
 import React from 'react'
 
 export default function Login() {
+
+    function onSignIn(googleUser) {
+        const profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+      }
+
     return (
         <div>
             <div className="g-signin2" data-onsuccess="onSignIn"></div>
