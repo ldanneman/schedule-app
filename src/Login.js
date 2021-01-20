8d930f87f5e2eeb2ee2de67e40c6ce2203f98135
Avishaignd <avishaignd@gmail.com>
2021-01-20 11:57:51 +0200

    login button is there

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Login() {
     return (
         <div>
-            <div class="g-signin2" data-onsuccess="onSignIn"></div>
+            <div className="g-signin2" data-onsuccess="onSignIn"></div>
         </div>
     )
 }
