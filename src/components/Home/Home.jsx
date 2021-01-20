0e9277c39927841f575eaa693dc7ce758c26e8cb
JakeRSA <jakenudels@gmail.com>
2021-01-20 23:17:35 +0200

    added styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import MainCards from "./MainCards";
 import "./home.css"
 import Login from "../Login";
+import { Container, Row } from "react-bootstrap";
+import logo from "../../shift-logo.png";
 
 function Home() {
   return (
-    <div className="home-container">
-      <MainCards />
+      <Container fluid className="w-75 mt-5">
+        <Row className="d-flex justify-content-center">
+          <img className="main-logo ml-2" src={logo} alt="logo" />
+        </Row>
+      <Row >
+        <h1 className="home-header p-3 mt-5">
+          Your time is valuable
+        </h1>
+      </Row>
+      <Row >
+        <h4 className="pt-5">Login to access our smart scheduling and say goodbye to late cancellations and missed appointments</h4>
+      </Row>
       <Login />
-    </div>
+      </Container>
   );
 }
 
