5316fd8c01d8e11493bfc7d6bbf010664a7fff39
YonatanGott <godiner@gmail.com>
2021-01-20 15:22:27 +0200

    api

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Home() {
+	return (
+		<Container fluid>
+			<Row>
+				<Col>
+					<h1> Welcome</h1>
+				</Col>
+			</Row>
+		</Container>
+	);
+}
+
+export default Home;
---
+++ src/components/NewAppointment.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewAppointment() {
+  return (
+    <div>
+      <h2>New appointment Form</h2>
+    </div>
+  );
+}
+
+export default NewAppointment;
